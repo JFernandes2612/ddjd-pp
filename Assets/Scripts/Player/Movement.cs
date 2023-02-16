06ad48eb6aaebd0cc8f684d773b0fc9dd86c9856
Mário Travassos <retchut@protonmail.ch>
2023-02-16 14:35:18 +0000

    feat: add simple player movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 input;
+    Rigidbody rb;
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputZ = Input.GetAxisRaw("Vertical");
+        float inputY = 0;
+        input = new Vector3(inputX, inputY, inputZ);
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(input * moveSpeed);
+    }
+}
