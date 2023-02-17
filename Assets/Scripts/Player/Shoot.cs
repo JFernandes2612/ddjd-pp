1020e22ed75c00529cfaae47d60f00008fbd31b0
Joel Fernandes <up201904977@fe.up.pt>
2023-02-17 22:59:32 +0000

    feat: add bullet shooting

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-
-    public GameObject shootPoint;
-    public GameObject bullet;
+    [SerializeField]
+    private GameObject shootPoint;
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private float bulletSpeed = 100.0f;
+    [SerializeField]
+    private GameObject playerModel;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +21,18 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            Instantiate(bullet, shootPoint.transform.position, bullet.transform.rotation);
+        playerModel.transform.rotation = Quaternion.identity;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            playerModel.transform.LookAt(new Vector3(raycastHit.point.x, playerModel.transform.position.y, raycastHit.point.z));
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletObject = Instantiate(bullet, shootPoint.transform.position, Quaternion.Euler(bullet.transform.rotation.eulerAngles + playerModel.transform.rotation.eulerAngles));
+            bulletObject.GetComponent<Rigidbody>().AddRelativeForce(-bulletObject.transform.forward * bulletSpeed);
         }
     }
+
 }
