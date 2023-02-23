c13f03f3958d5f1c0966f3f10737505224b121b3
Joel Fernandes <up201904977@fe.up.pt>
2023-02-23 16:10:59 +0000

    feat: add healthbar size scale with max health

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -11,14 +11,17 @@ public class HealthUI : MonoBehaviour
     private Health health;
     private ProgressBar healthBar;
     // Start is called before the first frame update
+    private float initialWidth;
     void Start()
     {
         healthBar = (ProgressBar)GetComponent<UIDocument>().rootVisualElement.Q("Health").Q("HealthBar");
+        initialWidth = 250.0f;
         health = player.GetComponent<Health>();
     }
 
     void FixedUpdate() {
         healthBar.highValue = health.getMaxHealth();
+        healthBar.style.width = new StyleLength(initialWidth + healthBar.highValue * 0.05f);
         healthBar.value = health.getCurrentHealth();
         healthBar.title =  healthBar.value + "/" + healthBar.highValue;
     }
