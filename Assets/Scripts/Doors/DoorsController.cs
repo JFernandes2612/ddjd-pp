92af9c62dea88d99cb8d4ac3cab4668d32f53028
Joel Fernandes <up201904977@fe.up.pt>
2023-02-27 23:35:20 +0000

    feat: add room spawning when door open

--- Assets/Scripts/Doors/DoorsController.cs
+++ Assets/Scripts/Doors/DoorsController.cs
@@ -7,10 +7,17 @@ public class DoorsController : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
+    [SerializeField]
+    private Transform roomSpawnPoint;
+
+    [SerializeField]
+    private GameObject[] rooms;
+
 
     // Distance from mid one door to dead center
     private float midDoorDistanceDeadCenter = 4.95f;
     private bool open = false;
+    private float corridorLength = 30.0f;
 
 
     private Transform door1;
@@ -33,27 +40,34 @@ public class DoorsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!open) {
+        if (!open)
+        {
             Vector3 doorsDeadCenterOnFloor = new Vector3(transform.position.x, player.transform.position.y, transform.position.z) + transform.forward * midDoorDistanceDeadCenter;
             float distanceToDoor = Vector3.Distance(player.transform.position, doorsDeadCenterOnFloor);
 
-            if (distanceToDoor < 10.0f) {
-                if (Input.GetKeyDown("e")) {
-                    //! Create explosion if have money check
+            if (distanceToDoor < 10.0f)
+            {
+                if (Input.GetKeyDown("e"))
+                {
+                    //! if have money check before next
 
                     open = true;
                     OpenDoors();
+
+                    //! Choose random Spawn Rate of Rooms
+                    Instantiate(rooms[0], roomSpawnPoint.position + -roomSpawnPoint.right * corridorLength, Quaternion.Euler(0, roomSpawnPoint.localEulerAngles.y + 135f, 0)).transform.parent = roomSpawnPoint;
                 }
             }
         }
 
         // Clamp Door Rotation Values
-        //! SILL HAVE TO FIX THIS
+        //! SILL HAVE TO FIX THIS SHIT KINDA BROKEY
         door1.localEulerAngles = Vector3.up * Mathf.Clamp(door1.localEulerAngles.y, 0f, 85f);
         door2.localEulerAngles = Vector3.up * Mathf.Clamp(door2.localEulerAngles.y, 95f, 180f);
     }
 
-    void OpenDoors() {
+    void OpenDoors()
+    {
         float toque = 850.0f;
         door1Rb.constraints &= ~RigidbodyConstraints.FreezeRotationY;
         door1Rb.AddTorque(Vector3.up * toque);
