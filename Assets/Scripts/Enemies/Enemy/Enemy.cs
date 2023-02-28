fe33e8b996262e1864525b34f9b3c2329ab92990
Mário Travassos <retchut@protonmail.ch>
2023-02-28 02:47:33 +0000

    feat: implement simple enemy hp bar

--- Assets/Scripts/Enemies/Enemy/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Enemy : Entity
 {
@@ -8,7 +9,13 @@ public abstract class Enemy : Entity
     protected EnemyController enemyController;
     protected GameObject player;
 
+    // enemy health par prefab
+    [SerializeField]
+    private GameObject enemyCanvasPrefab;
+
     protected int hp;
+    private GameObject enemyUI;
+    private Slider hpBar;
 
     protected abstract void SetFields();
 
@@ -17,12 +24,31 @@ public abstract class Enemy : Entity
     {
         player = GameObject.FindGameObjectWithTag("PlayerModel");
         SetFields();
+        CreateEnemyUI();
     }
 
     // Sets a reference to the EnemyController script component of the Enemy Controller GameObject
     public void SetEnemyController(EnemyController enemyController)
     {
         this.enemyController = enemyController;
+    }
+
+    private void CreateEnemyUI(){
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 0.7f);
+        Quaternion rot = Quaternion.Euler(75, 0, 0);
+        enemyUI = Instantiate(enemyCanvasPrefab, pos, rot);
+        enemyUI.transform.parent = transform;
+        hpBar = enemyUI.transform.GetChild(0).GetComponent<Slider>();
+        SetMaxHealth();
+    }
+
+    private void SetMaxHealth(){
+        hpBar.maxValue = hp;
+        hpBar.value = hp;
+    }
+
+    private void UpdateHealth(){
+        hpBar.value = hp;
     }
 
     // Input gathering
@@ -43,6 +69,7 @@ public abstract class Enemy : Entity
         if (collision.gameObject.CompareTag("PlayerProjectile"))
         {
             hp--;
+            UpdateHealth();
             if(hp == 0) Die();
         }
     }
