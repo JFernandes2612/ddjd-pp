4aaf887270849bad77b56f2f41211338b556cd6f
Mário Travassos <retchut@protonmail.ch>
2023-03-01 18:14:27 +0000

    fix: fix projectiles rotation

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pistol : Weapon
 {
     protected override void InstantiateProjectiles(){
-        GameObject spawnedProjectile = Instantiate(bulletPrefab, shootPoint.position, bulletPrefab.transform.rotation);
+        GameObject spawnedProjectile = Instantiate(bulletPrefab, shootPoint.position, shootPoint.transform.rotation * bulletPrefab.transform.rotation);
         spawnedProjectile.GetComponent<Projectile>().SetRange(range);
         spawnedProjectile.GetComponent<Rigidbody>().velocity = shootPoint.forward * bulletSpeed;
     }
