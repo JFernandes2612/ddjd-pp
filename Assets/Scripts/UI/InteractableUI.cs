a9c6f7d12cf6d2da79524feecfacd32d5893479b
Mário Travassos <retchut@protonmail.ch>
2023-03-02 12:56:08 +0000

    feat: make popup pop up

--- Assets/Scripts/UI/InteractableUI.cs
+++ Assets/Scripts/UI/InteractableUI.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class InteractableUI : MonoBehaviour
 {
     private Transform cameraTransform;
+    private Transform playerTransform;
+    private GameObject canvas;
+    [SerializeField]
+    private float radius;
     // Start is called before the first frame update
     void Start()
     {
-        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;        
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        canvas = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.LookAt(transform.position + cameraTransform.forward);
+        canvas.SetActive(PlayerInRadius());
+    }
+
+    private bool PlayerInRadius(){
+        return Mathf.Pow((playerTransform.position.x - transform.position.x), 2) + Mathf.Pow((playerTransform.position.z - transform.position.z), 2) <= Mathf.Pow(radius, 2);
     }
 }
