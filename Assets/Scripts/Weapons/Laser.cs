e09f9af7ee357164497869d0a12b0ab0aec61519
Joel Fernandes <up201904977@fe.up.pt>
2023-03-02 15:26:18 +0000

    refactor: change laser type weapons script name from pistol

--- Assets/Scripts/Weapons/Laser.cs
+++ Assets/Scripts/Weapons/Laser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pistol : Weapon
+public class Laser : Weapon
 {
     protected override void InstantiateProjectiles(){
         GameObject spawnedProjectile = Instantiate(bulletPrefab, shootPoint.position, shootPoint.transform.rotation * bulletPrefab.transform.rotation);
